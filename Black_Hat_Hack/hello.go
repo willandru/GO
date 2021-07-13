d660768d5a8093c6e7737e6de4c120c9b5ace89c
KaliW <kaliw@kali>
2021-07-13 02:00:42 -0500

    Golang is beautifulx2

--- Black_Hat_Hack/hello.go
+++ Black_Hat_Hack/hello.go
@@ -4,20 +4,37 @@ import (
 )
 func main(){
 	fmt.Println("Hello, Black Hat Hacker")
+
+	var x="Hola"
+	fmt.Println(x)
+
+	z := int(42)
+	fmt.Println(z)
+
+
+	fmt.Println("----SLICCE & MAP----")
+	var s = make([]string, 0)
+	var m= make(map[string]string)
+	s= append(s, "some string")
+	m["some key"] = "some value"
+
+
+	fmt.Println("slcie: ")
+	fmt.Println(s)
+	fmt.Println("Map: ")
+	fmt.Println(m)
+	fmt.Println("----POINTERS----")
+	var numero=int(45)
+	pointer:= &numero
+	fmt.Println("primer apuntador")
+	fmt.Println(pointer)
+	fmt.Println("retrive value stored: ")
+	fmt.Println(*pointer)
+	fmt.Println("cambiando el valor al numero..")
+	*pointer=100
+	fmt.Println(numero)
+
+	fmt.Println("----ESTRUCTURAS___")
+
+
 }
-var x= "Hello World"
-z:= int(42)
-
-var slice= make([]string,0)
-var mapa= make(map[string]string)
-
-s= append(s,"some string")
-
-mapa["some key"]= "some value"
-
-
-var count =int(43)
-
-ptr := &count
-*ptr =100
-fmt.Println(count)
