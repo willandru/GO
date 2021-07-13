927048420e6457d9d5e3d055cf1e58ba0dd94234
KaliW <kaliw@kali>
2021-07-13 00:25:40 -0500

    Golang is beautiful

---
+++ Black_Hat_Hack/hello.go
@@ -0,0 +1,23 @@
+package main
+import (
+	"fmt"
+)
+func main(){
+	fmt.Println("Hello, Black Hat Hacker")
+}
+var x= "Hello World"
+z:= int(42)
+
+var slice= make([]string,0)
+var mapa= make(map[string]string)
+
+s= append(s,"some string")
+
+mapa["some key"]= "some value"
+
+
+var count =int(43)
+
+ptr := &count
+*ptr =100
+fmt.Println(count)
